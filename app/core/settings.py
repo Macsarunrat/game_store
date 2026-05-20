64c21e2f55dd1c0c0ae10964d6f8cf952e0a4cdb
Sarunrat <tee20122538@gmail.com>
2026-05-20 15:39:48 +0700

    add env

--- app/core/settings.py
+++ app/core/settings.py
@@ -9,6 +9,23 @@ class Settings(BaseSettings):
     stripe_webhook : str
     domain : str = "http://localhost:8000"
 
+    POSTGRES_URL:str
+
+    ALGORITHM:str
+    ACCESS_TOKEN_EXPIRE_MINUTES:int
+    SECRET_KEY:str
+
+    POSTGRES_USER:str
+    POSTGRES_PASSWORD:str
+    POSTGRES_DB:str
+
+    REDIS_URL:str
+
+    MAIL_USERNAME:str
+    MAIL_PASSWORD:str
+    MAIL_FROM_NAME:str
+
+
     model_config = SettingsConfigDict(env_file='...env')
 
 settings = Settings()
